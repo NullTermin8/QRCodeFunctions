2596fdc9fe56f4f08370a18555905a83d39a3bae
modernNeo <j_manshad@sfu.ca>
2018-01-14 02:13:31 -0800

    adding lastest code for Mirch

--- qrCodeCreator/qr_creator.js
+++ qrCodeCreator/qr_creator.js
@@ -1,57 +1,32 @@
 
-function createQRCode(fromId = 0, cost = 0 , eventID = 'NA', TTL= 0){
-  if (fromId == 0){
-    callback(new Error('No From ID has been specified...'))
+function createQRCode(fromId, cost , eventID, TTL){
+  if (isNaN(fromId) || fromId == '' || fromId.localeCompare('fromID')){
+    document.getElementById('errorMessage').innerHTML = 'No proper FromID has been specified...';
+    return
     //error, no fromID specified
   }
-  if (cost < 0){
-    callback(new Error('Negative cost has been specified...'))
-    //error, cost is negative
+
+  if (isNaN(cost) || cost == '' || cost == 'cost' || cost < 0){
+    document.getElementById('errorMessage').innerHTML = 'No proper cost has been specified...';
+    return
+    //error, no fromID specified
   }
-  if (eventID == 'NA'){
-    callback(new Error('No eventID has been specified...'))
+  console.log("eventID="+eventID)
+  if (eventID.localeCompare('eventID') || eventID == ''){
+    document.getElementById('errorMessage').innerHTML = 'No eventID has been specified...';
+    return
     //error, no eventID
   }
-  if (TTL == 0){
-    callback(new Error("No TTL has been specified..."))
+  if (isNaN(TTL) || TTL == '' || TTL == 'TTL' || TTL < 0){
+    document.getElementById('errorMessage').innerHTML = "No proper TTL has been specified";
+    return
     //error, no TTL
   }
-
+  console.log("hello")
   var obj = '\{\"fromID\" : \"'+fromId+'\",'
        +'\"cost\"  : \"'+cost+'\",'
        +'\"eventID\" : \"'+eventID+'\",'
        +'\"TTL : \"'+TTL+'\"\}';
   url="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+obj
-  var anHttpRequest = new XMLHttpRequest();
-  anHttpRequest.onreadystatechange = function() { 
-    if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
-      callback(anHttpRequest.responseText);
-  }
-  anHttpRequest.open( "GET", url, true );            
-  anHttpRequest.send( null );
+  document.getElementById('myImage').src=url;
 }
-var qrcode = new QRCode("qrcode");
-
-function makeCode () {      
-    var elText = document.getElementById("text");
-    
-    if (!elText.value) {
-        alert("Input a text");
-        elText.focus();
-        return;
-    }
-    
-    qrcode.makeCode(elText.value);
-}
-
-makeCode();
-
-$("#text").
-    on("blur", function () {
-        makeCode();
-    }).
-    on("keydown", function (e) {
-        if (e.keyCode == 13) {
-            makeCode();
-        }
-    });
